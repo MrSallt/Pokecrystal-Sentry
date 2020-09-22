a1e1b92a262560c9cca6fd797a5140bdc5a28859
Maxime <68646464+MrSallt@users.noreply.github.com>
2020-09-22 08:16:22 +0200

    Update main.asm

--- main.asm
+++ main.asm
@@ -724,6 +724,10 @@ SECTION "Crystal Events", ROMX
 INCLUDE "engine/events/battle_tower/load_trainer.asm"
 INCLUDE "engine/events/odd_egg.asm"
 
+SECTION "16-bit ID stuff", ROMX
+
+INCLUDE "engine/16/table_functions.asm"
+
 
 SECTION "Stadium 2 Checksums", ROMX[$7DE0], BANK[$7F]
 
