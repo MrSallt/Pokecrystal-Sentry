767b6676d0c04fa860b97e097d4361f2d9c0cf8f
Maxime <68646464+MrSallt@users.noreply.github.com>
2020-10-28 10:07:18 +0100

    Intégration d'un système de pokémon variant
    L'idée est d'implanter un système permettant à un pokémon d'avoir plusieur sprites, mais aussi plusieurs types ou encore plusieurs movepools en fonction de sa variable.

---
+++ data/pokemon/variant_form.asm
@@ -0,0 +1,31 @@
+; Data tables that vary for forms list normal species data up to 255 (EGG),
+; then continue with entries for these species+form combinations.
+
+CosmeticSpeciesAndFormTable:
+	db UNOWN,     UNOWN_B_FORM
+	db UNOWN,     UNOWN_C_FORM
+	db UNOWN,     UNOWN_D_FORM
+	db UNOWN,     UNOWN_E_FORM
+	db UNOWN,     UNOWN_F_FORM
+	db UNOWN,     UNOWN_G_FORM
+	db UNOWN,     UNOWN_H_FORM
+	db UNOWN,     UNOWN_I_FORM
+	db UNOWN,     UNOWN_J_FORM
+	db UNOWN,     UNOWN_K_FORM
+	db UNOWN,     UNOWN_L_FORM
+	db UNOWN,     UNOWN_M_FORM
+	db UNOWN,     UNOWN_N_FORM
+	db UNOWN,     UNOWN_O_FORM
+	db UNOWN,     UNOWN_P_FORM
+	db UNOWN,     UNOWN_Q_FORM
+	db UNOWN,     UNOWN_R_FORM
+	db UNOWN,     UNOWN_S_FORM
+	db UNOWN,     UNOWN_T_FORM
+	db UNOWN,     UNOWN_U_FORM
+	db UNOWN,     UNOWN_V_FORM
+	db UNOWN,     UNOWN_W_FORM
+	db UNOWN,     UNOWN_X_FORM
+	db UNOWN,     UNOWN_Y_FORM
+	db UNOWN,     UNOWN_Z_FORM
+VariantSpeciesAndFormTable:
+	db 0 ; end
